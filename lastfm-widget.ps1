1b1dfbeeb8282ee6ee14cbcd80bedbc8f882190c
damsmcfly <zobzz666@gmail.com>
2025-12-25 14:36:30 +0100

    Update lastfm-widget.ps1
    added top artists, top albums, top tracks

--- lastfm-widget.ps1
+++ lastfm-widget.ps1
@@ -1,28 +1,67 @@
 $ProgressPreference = 'SilentlyContinue'
 
-$r = Invoke-RestMethod 'https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=<___CHANGE_USERNAME_HERE___>&api_key=<___CHANGE_API_KEY_HERE___>&format=json'
+# Fetch user info
+$userInfo = Invoke-RestMethod 'https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=<CHANGE_USERNAME_HERE>&api_key=<CHANGE_API_KEY_HERE>&format=json'
 
-$playcount = 	[int64]$r.user.playcount
-$artist_count = [int64]$r.user.artist_count
-$track_count = 	[int64]$r.user.track_count
-$album_count = 	[int64]$r.user.album_count
-$registered = 	[int64]$r.user.registered.unixtime
-$username  = 	$r.user.name
+# Fetch top artists
+$topArtists = Invoke-RestMethod 'https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=<CHANGE_USERNAME_HERE>&api_key=<CHANGE_API_KEY_HERE>&format=json&limit=5&period=7day'
 
-# format number with spaces
-$playcountFormatted = 		'{0:N0}' -f $playcount -replace ',', " "
-$artist_countFormatted = 	'{0:N0}' -f $artist_count -replace ',', " "
-$track_countFormatted = 	'{0:N0}' -f $track_count -replace ',', " "
-$album_countFormatted = 	'{0:N0}' -f $album_count -replace ',', " "
+# Fetch top albums
+$topAlbums = Invoke-RestMethod 'https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=<CHANGE_USERNAME_HERE>&api_key=<CHANGE_API_KEY_HERE>&format=json&limit=5&period=7day'
 
-# convert unix time → YYYY-MM-DD
+# Fetch top tracks
+$topTracks = Invoke-RestMethod 'https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=<CHANGE_USERNAME_HERE>&api_key=<CHANGE_API_KEY_HERE>&format=json&limit=5&period=7day'
+
+# Extract user info
+$playcount =       [int64]$userInfo.user.playcount
+$artist_count =    [int64]$userInfo.user.artist_count
+$track_count =     [int64]$userInfo.user.track_count
+$album_count =     [int64]$userInfo.user.album_count
+$registered =      [int64]$userInfo.user.registered.unixtime
+$username =        $userInfo.user.name
+
+# Format numbers with spaces
+$playcountFormatted =       '{0:N0}' -f $playcount -replace ',', " "
+$artist_countFormatted =   '{0:N0}' -f $artist_count -replace ',', " "
+$track_countFormatted =    '{0:N0}' -f $track_count -replace ',', " "
+$album_countFormatted =    '{0:N0}' -f $album_count -replace ',', " "
+
+# Convert unix time → YYYY-MM-DD
 $registeredDate = [DateTimeOffset]::FromUnixTimeSeconds($registered).ToString('yyyy-MM-dd')
 
-@{
-    playcount = 	$playcountFormatted
-    artist_count = 	$artist_countFormatted
-    track_count = 	$track_countFormatted
-    album_count = 	$album_countFormatted
-    username  = 	$username
-    registered  = 	$registeredDate
-} | ConvertTo-Json -Compress
+# Format top 5 artists as a string
+$top5ArtistsString = ""
+foreach ($artist in $topArtists.topartists.artist) {
+    $top5ArtistsString += "- $($artist.name) ($($artist.playcount))<br>"
+}
+$top5ArtistsString = $top5ArtistsString.TrimEnd("<br>")
+
+# Format top 5 albums as a string
+$top5AlbumsString = ""
+foreach ($album in $topAlbums.topalbums.album) {
+    $top5AlbumsString += "- $($album.name) by $($album.artist.name) ($($album.playcount))<br>"
+}
+$top5AlbumsString = $top5AlbumsString.TrimEnd("<br>")
+
+# Format top 5 tracks as a string
+$top5TracksString = ""
+foreach ($track in $topTracks.toptracks.track) {
+    $top5TracksString += "- $($track.name) by $($track.artist.name) ($($track.playcount))<br>"
+}
+$top5TracksString = $top5TracksString.TrimEnd("<br>")
+
+# Create output object
+$output = @{
+    playcount =         $playcountFormatted
+    artist_count =      $artist_countFormatted
+    track_count =       $track_countFormatted
+    album_count =       $album_countFormatted
+    username =          $username
+    registered =        $registeredDate
+    top_artists =       $top5ArtistsString
+    top_albums =        $top5AlbumsString
+    top_tracks =        $top5TracksString
+}
+
+# Output as JSON
+$output | ConvertTo-Json -Compress
